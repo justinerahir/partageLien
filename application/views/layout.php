3ac6ed9de23bdf7d037b446f8fe3d47417e8f2b2
justinerahir <justine.rahir@gmail.com>
2012-10-24 14:20:40 +0200

    partageDeLien
    CSS

--- application/views/layout.php
+++ application/views/layout.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title> <?= $titre ?> </title>
-	<link rel="stylesheet" type="text/css" href="site_url().CSS_DIR.'styles.css'">
+	<link rel="stylesheet" type="text/css" href="<?= site_url().CSS_DIR.'styles.css'?>">
 </head>
 <body>
 	<div>
